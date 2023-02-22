c761772ee904a4ea470d566155996b5be5fef283
Katarzyna KLEPACKA <kashiaa@gmail.com>
2023-02-22 10:00:02 +0100

    lala

--- cypress/e2e/logintest.cy.ts
+++ cypress/e2e/logintest.cy.ts
@@ -1,7 +1,7 @@
 import { LoginPage } from "../pages/LoginPage"
 
 let loginPage = new LoginPage
-it('login test', function() {
+it('login#test', function() {
     loginPage.navigate('https://trytestingthis.netlify.app/');
     loginPage.enterUsrName('test');
     loginPage.enterPwd('test');
